679fbbe1538f97acf937d56aabe6fdebb051b8a3
ryuj-h <dbwlsgh57@naver.com>
2022-12-31 15:12:37 +0900

    cpprest를 사용한 서버 임포트

---
+++ serverPC/main.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cpprest/http_listener.h>
+
+using namespace std;
+using namespace web::http;
+using namespace web::http::experimental::listener;
+
+int main() {
+	http_listener listener(U("http://localhost:7890"));
+	listener.open().then([&listener]() {cout << (U("\n start \n")); }).wait();
+	listener.support(methods::GET, [](http_request req) {
+		req.reply(status_codes::OK, U("hello world!"));
+	});
+	while (true);
+	listener.close();
+	return 0;
+}
